b4051bab26d64350658598012742940f04fa9b76
Александр <ihumster@ihumster.ru>
2015-08-21 15:20:23 +0500

    Update 1cloud.ru REST API Use.ps1

--- 1cloud.ru REST API Use.ps1
+++ 1cloud.ru REST API Use.ps1
@@ -13,6 +13,24 @@ $serversList = Invoke-RestMethod -Method Get -Uri $uri -Headers $header -Content
 #В переменной $serversList получим массив элементов PSCustomObject или один элемент PSCustomObject
 #посмотреть поля объекта можно в документации https://1cloud.ru/api/server/serverlist
 
+#Создать сервер
+$uri = "https://api.1cloud.ru/server"
+$serverSet = '{"Name":"testAPI","CPU":1,"RAM":1024,"HDD":40,"imageID":1,"HDDType":"SSD","IsHighPerformance":true}'
+$res = = Invoke-RestMethod -Method Post -Uri $uri -Headers $header -ContentType 'application/json' -Body $serverSet
+#В переменной $serverSet находится набор переменных для задания базовых параметорв виртуального сервера
+#В переменной $res возвращаются выходные параметры сервера: ID, Name и т.п.
+#Подробнее о входных и выходных параметрах можно узнать в документации https://1cloud.ru/api/server/createserver
+
+#Выключить сервер
+$id = "123"
+$uri = "https://api.1cloud.ru/server/$id/action"
+$action = '{"Type":"PowerOff"}'
+$res = = Invoke-RestMethod -Method Post -Uri $uri -Headers $header -ContentType 'application/json' -Body $action
+#В переменной $id задается номер сервера, получить который можно с помощью "Получить список серверов"
+#В переменной $action задается тип действия (в данном примере выключение сервера)
+#В переменной $res возвращается результат действия
+#Подробнее о типах действий и выходных параметрах можно узнать в документации https://1cloud.ru/api/server/servercreateaction
+
 #Получить список сетей
 $uri = "https://api.1cloud.ru/network"
 $networksList = Invoke-RestMethod -Method Get -Uri $uri -Headers $header -ContentType 'application/json'
